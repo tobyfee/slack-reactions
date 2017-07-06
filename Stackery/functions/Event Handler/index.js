7204307a6b7f3fde1900f457eb5f428bb327ddce
Stackery <support@stackery.io>
2017-07-06 21:02:15 +0000

    Stackery revision

--- Stackery/functions/Event Handler/index.js
+++ Stackery/functions/Event Handler/index.js
@@ -1,9 +1,7 @@
 const stackery = require('stackery')
 
 module.exports = function handler(message) {
-  console.log('We\'re in the handler')
-  console.log(message.body);
-  console.log('That was the message')
-  console.log('Body type is ', typeof(message.body))
-  return JSON.parse(message).challenge;
+  let body = JSON.parse(message.body.toString());
+  console.log(body);
+  return body.challenge;
 }
