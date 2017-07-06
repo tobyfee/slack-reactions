72c5257bb561d209e0fa97ed334fbd7c04b72f55
Stackery <support@stackery.io>
2017-07-06 21:17:02 +0000

    Stackery revision

--- Stackery/functions/Test/index.js
+++ Stackery/functions/Test/index.js
@@ -2,6 +2,8 @@ const stackery = require('stackery')
 
 module.exports = function handler(message) {
   let body = message.body.toString();
+  console.log(body);
+  console.log(typeof(body));
 
   return { text: body.text.split('').reverse().join('') }
 }
