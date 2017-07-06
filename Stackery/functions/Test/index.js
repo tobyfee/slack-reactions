027dacbdb6216ea0b194a1235340ea6209c51779
Stackery <support@stackery.io>
2017-07-06 21:58:52 +0000

    Stackery revision

--- Stackery/functions/Test/index.js
+++ Stackery/functions/Test/index.js
@@ -2,7 +2,7 @@ const stackery = require('stackery')
 
 module.exports = function handler(message) {
   let body = message.body.toString();
-  let fields = body.split(' ').map((k) => k.split('='));
+  let fields = body.split('&').map((k) => k.split('='));
   console.log(fields);
   let text = fields.find(field => field[0] == 'text');
   console.log('text is ', text);
