6da905da8941eac243d5cc8b9e60a57c4b696ef3
Stackery <support@stackery.io>
2017-07-06 22:03:41 +0000

    Stackery revision

--- Stackery/functions/Test/index.js
+++ Stackery/functions/Test/index.js
@@ -5,5 +5,7 @@ module.exports = function handler(message) {
   let fields = body.split('&').map((k) => k.split('='));
   console.log(fields);
   let text = fields.find(field => field[0] == 'text')[1];
-  return { text: text.split('').reverse().join('') }
+  let response = { text: text.split('').reverse().join('') };
+  console.log('response is', response);
+  return response;
 }
