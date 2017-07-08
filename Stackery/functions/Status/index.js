c50476b2a928acf118f37e7234f7ab1d86f83565
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 16:33:12 -0700

    Respond with reaction status

--- Stackery/functions/Status/index.js
+++ Stackery/functions/Status/index.js
@@ -3,9 +3,7 @@ const stackery = require('stackery')
 module.exports = function status(message) {
   let body = message.body.toString();
   let fields = body.split('&').map((k) => k.split('='));
-  console.log(fields);
-  let action = 'select';
-  let where = { id: event.user };
-  let user = stackery.output({ action, where, increment });
-  console.log('user is ', user);
+  let id = fields.find(field => field[0] == 'user_id')[1];
+  let user = stackery.output({ action: 'select', where: { id } });
+  return JSON.stringify(user);
 }
