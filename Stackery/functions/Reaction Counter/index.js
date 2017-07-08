6405b46faeb9721102ffee441c83b82e20d41f54
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 14:08:22 -0700

    Playing with output function

--- Stackery/functions/Reaction Counter/index.js
+++ Stackery/functions/Reaction Counter/index.js
@@ -13,6 +13,7 @@ const stackery = require('stackery')
 //                 "event_ts": "1499542683.228446"
 
 module.exports = function reactionCounter(event) {
+  console.log('event is ', event);
   let clause = { user: { condition: '=', value: event.item_user } };
   console.log('clause is ', clause);
   let user = stackery.output({ action: 'select', where: clause }) || { user: event.item_user, reactions: [] };
