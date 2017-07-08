f78b78625be5091dbbb60e7cd8acb13614406e75
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 15:56:13 -0700

    Playing with output function

--- Stackery/functions/Reaction Counter/index.js
+++ Stackery/functions/Reaction Counter/index.js
@@ -3,7 +3,7 @@ const stackery = require('stackery')
 module.exports = function reactionCounter(message) {
   let event = JSON.parse(message.body.toString()).event;
   let action = 'update';
-  let where = { id: { condition: '=', value: event.item_user } };
+  let where = { id: event.item_user };
   let increment = { [event.reaction]: 1 };
   console.log('before');
   console.log({ action, where, increment });
