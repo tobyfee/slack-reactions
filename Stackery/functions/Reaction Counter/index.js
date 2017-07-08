1d9293d27cb4374edee3f4b7c70de6db634d7169
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 15:47:18 -0700

    Playing with output function

--- Stackery/functions/Reaction Counter/index.js
+++ Stackery/functions/Reaction Counter/index.js
@@ -1,24 +1,12 @@
 const stackery = require('stackery')
 
-//     "event": {
-//               "type": "reaction_added",
-//                 "user": "U5UTLD724",
-//                 "item": {
-//                               "type": "message",
-//                                 "channel": "C5WCUKPU7",
-//                                 "ts": "1499379574.375919"
-//                           },
-//                 "reaction": "joy",
-//                 "item_user": "U5UTLD724",
-//                 "event_ts": "1499542683.228446"
-
 module.exports = function reactionCounter(message) {
   let event = JSON.parse(message.body.toString()).event;
-  let clause = { id: { condition: '=', value: event.item_user } };
-  console.log('message is ', { action: 'select', where: clause });
-  let user = stackery.output({ action: 'select', where: clause }) || { id: event.item_user, reactions: [] };
-  console.log('user is ', user);
-  user.reactions.push(event.reaction);
-  console.log(user);
-  stackery.output({ action: 'put', record: user });
+  let action = 'update';
+  let where = { id: { condition: '=', value: event.item_user } };
+  let increment = { [event.reaction]: 1 };
+  console.log('before');
+  console.log({ action, where, increment });
+  console.log('after');
+  let user = stackery.output({ action, where, increment });
 }
