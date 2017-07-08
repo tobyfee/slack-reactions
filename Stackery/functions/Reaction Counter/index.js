84376c01c99ab778fa768a8b024f0e6b1105dfcd
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 12:30:55 -0700

    Playing with output function

--- Stackery/functions/Reaction Counter/index.js
+++ Stackery/functions/Reaction Counter/index.js
@@ -1,6 +1,5 @@
 const stackery = require('stackery')
 
 module.exports = function reactionCounter(message) {
-  console.log('hello there, from inside the reaction counter');
-  stackery.output('testing 123');
+  stackery.output({ action: 'insert', record: {} });
 }
