4eeaa5fbd1489ba3a9a7ccd1d11f3033631563fc
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 16:38:04 -0700

    Adding log statements

--- Stackery/functions/Status/index.js
+++ Stackery/functions/Status/index.js
@@ -5,5 +5,7 @@ module.exports = function status(message) {
   let fields = body.split('&').map((k) => k.split('='));
   let id = fields.find(field => field[0] == 'user_id')[1];
   let user = stackery.output({ action: 'select', where: { id } });
+  console.log('asking for', { action: 'select', where: { id } });
+  console.log('user is', user);
   return JSON.stringify(user);
 }
