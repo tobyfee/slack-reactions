933e99dc28649658cdd88628bd84fb45326df2e7
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-09 15:53:42 -0700

    Adding log

--- Stackery/functions/Reaction Counter/index.js
+++ Stackery/functions/Reaction Counter/index.js
@@ -4,6 +4,7 @@ module.exports = function reactionCounter(message) {
   let event = JSON.parse(message.body.toString()).event;
   let action = 'update';
   let where = { id: event.item_user };
+  console.log('incrementing', event.reaction, 'by', 1);
   let increment = { [event.reaction]: 1 };
   stackery.output({ action, where, increment });
 }
