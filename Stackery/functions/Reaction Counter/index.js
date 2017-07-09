ac1d5c757e94a40152fbd492185d1fd444499d1d
Stackery <support@stackery.io>
2017-07-09 23:33:17 +0000

    Stackery revision

--- Stackery/functions/Reaction Counter/index.js
+++ Stackery/functions/Reaction Counter/index.js
@@ -2,6 +2,7 @@ const stackery = require('stackery')
 
 module.exports = function reactionCounter(message) {
   let event = JSON.parse(message.body.toString()).event;
+  console.log('event is', event);
   let action = 'update';
   let where = { id: event.item_user };
   console.log('incrementing', event.reaction, 'by', 1);
