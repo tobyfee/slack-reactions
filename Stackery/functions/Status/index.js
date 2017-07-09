d9b9f2a837686d838fc23dedf63a962be462310c
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 17:13:02 -0700

    Stringify

--- Stackery/functions/Status/index.js
+++ Stackery/functions/Status/index.js
@@ -4,8 +4,5 @@ module.exports = function status(message) {
   let body = message.body.toString();
   let fields = body.split('&').map((k) => k.split('='));
   let id = fields.find(field => field[0] == 'user_id')[1];
-  return stackery.output({ action: 'select', where: { id } }).then(result => {
-    console.log(result);
-    return result
-  });
+  return stackery.output({ action: 'select', where: { id } }).then(result => JSON.stringify(result.records[0]));
 }
