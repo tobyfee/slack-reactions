3ac573015f97f0f6e061e2db7c5206475f598063
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-08 17:08:18 -0700

    Handle promise

--- Stackery/functions/Status/index.js
+++ Stackery/functions/Status/index.js
@@ -4,7 +4,8 @@ module.exports = function status(message) {
   let body = message.body.toString();
   let fields = body.split('&').map((k) => k.split('='));
   let id = fields.find(field => field[0] == 'user_id')[1];
-  let user = Promise.resolve(stackery.output({ action: 'select', where: { id } }));
-  console.log(user);
-  return JSON.stringify(user);
+  return stackery.output({ action: 'select', where: { id } }).then(result => {
+    console.log(result);
+    return result
+  });
 }
