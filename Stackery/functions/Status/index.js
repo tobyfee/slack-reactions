cc892a29aa61bfd5c859793404ab324400e8c6d6
Stackery <support@stackery.io>
2017-07-10 07:19:51 +0000

    Stackery revision

--- Stackery/functions/Status/index.js
+++ Stackery/functions/Status/index.js
@@ -4,5 +4,14 @@ module.exports = function status(message) {
   let body = message.body.toString();
   let fields = body.split('&').map((k) => k.split('='));
   let user = fields.find(field => field[0] == 'user_id')[1];
-  return stackery.output({ action: 'select', where: { user } }).then(result => JSON.stringify(result[0].records));
+  return stackery.output({ action: 'select', where: { user } }).then(result => 
+    JSON.stringify(result[0].records).reduce((rxns, rxn) => {
+      if (rxn.reaction in rxns) {
+        rxns[rxn.reaction] += 1; 
+      } else {
+        rxns[rxn.reaction] = 1;
+      }
+      return rxns;
+    })
+  );
 }
