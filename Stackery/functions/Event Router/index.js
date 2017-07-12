206101ac5bed40c6e6175e9ac205159bc483b05e
Stackery <support@stackery.io>
2017-07-12 22:35:30 +0000

    Stackery revision

--- Stackery/functions/Event Router/index.js
+++ Stackery/functions/Event Router/index.js
@@ -2,15 +2,19 @@ const stackery = require('stackery')
 
 module.exports = function eventRouter(message) {
   const body = JSON.parse(message.body.toString());
-  console.log('body is ', body);
-  switch (body.type || body.event.type) {
+  switch (body.type) {
     case 'url_verification':
       return stackery.output(body, { port: 0 });
-    case 'reaction_added': 
-      return stackery.output(body.event, { port: 1 });
-    case 'reaction_removed':
-      return stackery.output(body.event, { port: 2 });
+    case 'event_callback': 
+      switch (body.event.type) {
+        case 'reaction_added':
+          return stackery.output(body.event, { port: 1 });
+        case 'reaction_removed':
+          return stackery.output(body.event, { port: 1 });
+        default:
+          throw new Error('Unrecognized event callback: ', body.event);
+      }
     default:
-      throw new Error('Unrecognized event: ', body);
+      throw new Error('Unrecognized event type: ', body);
   }
 }
