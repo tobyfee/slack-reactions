14013a4daa7a2019cfe4c5066f36f9e1cde6b78b
Stackery <support@stackery.io>
2017-07-12 04:53:42 +0000

    Stackery revision

--- Stackery/functions/Reaction Message Builder/index.js
+++ Stackery/functions/Reaction Message Builder/index.js
@@ -2,7 +2,7 @@ const stackery = require('stackery')
 
 module.exports = function reactionMessageBuilder({ users }) {
   let text = '';
-  for (let user in users.sort((u1, u2) => u1.reactions.length - u2.reactions.length)) {
+  for (let user in users) {
     text += `<@${user.id}>: `;
     // let reactions = user.reactions.sort((r1, r2) => r1.count < r2.count);
     // for (let reaction in reactions) {
