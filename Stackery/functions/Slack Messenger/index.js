25324a9395a00b6903a1cd3408a1baeaaa1f7875
Stackery <support@stackery.io>
2017-07-12 02:49:06 +0000

    Stackery revision

--- Stackery/functions/Slack Messenger/index.js
+++ Stackery/functions/Slack Messenger/index.js
@@ -4,7 +4,7 @@ const querystring = require('querystring');
 
 module.exports = function slackMessenger(message = 'Testing...') {
   const postData = querystring.stringify({
-    'token': 'bjl16TxNV1KRSoQKBd4StXhZ',
+    'token': 'f7e228831191bcc09313489761afa6a1',
     'channel': 'general',
     'text': message,
   });
