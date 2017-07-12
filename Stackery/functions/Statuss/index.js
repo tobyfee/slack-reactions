488a37a90d158b57d52bcafe79c8b0a7f4aa5aae
Stackery <support@stackery.io>
2017-07-12 04:40:42 +0000

    Stackery revision

--- Stackery/functions/Statuss/index.js
+++ Stackery/functions/Statuss/index.js
@@ -2,7 +2,7 @@ const stackery = require('stackery');
 
 module.exports = function status(message) {
   return stackery.output({ action: 'select' }, { port: 0 }).then((result) => {
-    return stackery.output(JSON.stringify(result[0].records), { port: 1 }).then((result) =>  {
+    return stackery.output(result[0].records, { port: 1 }).then((result) =>  {
       return result;
     });
   });
