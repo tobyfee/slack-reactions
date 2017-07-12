876ed486a8738620891cfaa7c17d908bc7830c33
Stackery <support@stackery.io>
2017-07-12 05:43:05 +0000

    Stackery revision

--- Stackery/functions/Event Handler/index.js
+++ Stackery/functions/Event Handler/index.js
@@ -3,6 +3,7 @@ const stackery = require('stackery')
 module.exports = function reactionCounter(message) {
   const event = JSON.parse(message.body.toString()).event;
   return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
+    console.log('records are', result);
     let user = result[0].records[0] ? result[0].records[0] : { id: event.item_user, reactions: [] };
     user.reactions.push({ type: event.reaction });
     console.log('putting', user);
