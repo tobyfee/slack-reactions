277c868f320dc906b351f41cf807653df4bc742f
Stackery <support@stackery.io>
2017-07-12 22:46:36 +0000

    Stackery revision

--- Stackery/functions/Event Router/index.js
+++ Stackery/functions/Event Router/index.js
@@ -2,6 +2,7 @@ const stackery = require('stackery')
 
 module.exports = function eventRouter(message) {
   const body = JSON.parse(message.body.toString());
+  console.log('Routing: ', body);
   switch (body.type) {
     case 'url_verification':
       return stackery.output(body, { port: 0 });
