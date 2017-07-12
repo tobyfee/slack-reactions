439b5bb519edbb75d69fa939fdd17467c5963bff
Stackery <support@stackery.io>
2017-07-12 23:49:22 +0000

    Stackery revision

--- Stackery/functions/Add Reaction/index.js
+++ Stackery/functions/Add Reaction/index.js
@@ -3,6 +3,7 @@ const stackery = require('stackery')
 module.exports = function reactionCounter(event) {
   return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
     console.log('records are', result);
+    console.log('event is', event);
     let user = result[0].records[0] ? result[0].records[0] : { id: event.item_user, reactions: [] };
     user.reactions.push({ type: event.reaction });
     console.log('putting', user);
