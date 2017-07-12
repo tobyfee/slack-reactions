757ec4ee9ff7ed9333f0e5a471fb636a65c45331
Stackery <support@stackery.io>
2017-07-12 02:11:24 +0000

    Stackery revision

--- Stackery/functions/Slack Messenger/index.js
+++ Stackery/functions/Slack Messenger/index.js
@@ -16,9 +16,24 @@ module.exports = function slackMessenger(message = 'Testing...') {
       'Content-Length': Buffer.byteLength(postData)
     }
   };
-  console.log('making request')
-  const req = http.request(options);
-  console.log('writing to request')
+  const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+  
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+  
+  console.log('writing data')
+
   req.write(postData);
   console.log('ending request')
   req.end();
