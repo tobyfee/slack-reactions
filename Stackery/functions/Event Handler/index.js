9984f25b48960a5c80777d765e60939337b43098
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-11 21:18:22 -0700

    Fix user creation

--- Stackery/functions/Event Handler/index.js
+++ Stackery/functions/Event Handler/index.js
@@ -3,8 +3,7 @@ const stackery = require('stackery')
 module.exports = function reactionCounter(message) {
   const event = JSON.parse(message.body.toString()).event;
   return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
-    console.log(result);
-    let user = result[0] ? result[0].record : { id: event.item_user };
+    let user = result[0].records[0] ? result[0].records[0] : { id: event.item_user };
     user[event.reaction] = user[event.reaction] ? user[event.reaction] + 1 : 1;
     stackery.output({ action: 'put', record: user }).then((result) => {
       return {};
