245c32f4178c577b60feb8d089ba45de6f3a37df
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-11 20:55:49 -0700

    Switching to user-based references

--- Stackery/functions/Event Handler/index.js
+++ Stackery/functions/Event Handler/index.js
@@ -1,9 +1,8 @@
 const stackery = require('stackery')
 
 module.exports = function reactionCounter(message) {
-  let event = JSON.parse(message.body.toString()).event;
-  let action = 'insert';
-  let record = { user: event.item_user, timestamp: parseFloat(event.event_ts), reaction: event.reaction  };
-  stackery.output({ action, record });
-  return { body: 'Event Recorded' };
+  const event = JSON.parse(message.body.toString()).event;
+  return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
+    console.log(result);
+  });
 }
