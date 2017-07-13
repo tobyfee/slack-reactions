ac1b5493930698ddf43b25c12eee6d2fc63b9c1d
Stackery <support@stackery.io>
2017-07-13 01:53:38 +0000

    Stackery revision

--- Stackery/functions/Remove Reaction/index.js
+++ Stackery/functions/Remove Reaction/index.js
@@ -2,6 +2,7 @@ const stackery = require('stackery')
 
 module.exports = function reactionCounter(event) {
   return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
+    console.log('result from here is', result)
     let user = result[0].records[0] ? result[0].records[0] : { id: event.item_user, reactions: [] };
     console.log('Event is ', event);
     return {}
