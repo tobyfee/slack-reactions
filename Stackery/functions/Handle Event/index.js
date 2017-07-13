96e20cb97d8b5637a077931518e4ff79cdd9a905
Stackery <support@stackery.io>
2017-07-13 22:35:49 +0000

    Stackery revision

--- Stackery/functions/Handle Event/index.js
+++ Stackery/functions/Handle Event/index.js
@@ -6,11 +6,9 @@ module.exports = function reactionCounter(event) {
   const where = { id: event.item_user };
   switch (event.type) {
     case 'reaction_added':
-      let increment = { ['_' + reaction.type]: 1 };
-      return stackery.output({ action , where, increment });
+      return stackery.output({ action , where, increment: { ['_' + reaction.type]: 1 } });
     case 'reaction_removed':
-      let increment = { ['_' + reaction.type]: -1 };
-      return stackery.output({ action , where, increment });
+      return stackery.output({ action , where, increment: { ['_' + reaction.type]: -1 });
     default:
       throw new Error('Unknown event type: ' + event.type);
   }
