cec77b7e3c595786e555ce9969a240173f0b7938
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-12 20:30:13 -0700

    Put report generation into status function

--- Stackery/functions/Reaction Message Builder/index.js
+++ Stackery/functions/Reaction Message Builder/index.js
@@ -1,15 +1,4 @@
 const stackery = require('stackery')
 
 module.exports = function reactionMessageBuilder({ users }) {
-  let text = '';
-  console.log('users are', users);
-  for (let user of users) {
-    console.log('reactions are', user.reactions);
-    text += `<@${user.id}>: `;
-    for (let reaction of user.reactions) {
-      text += `:${reaction.type}:`;
-    }
-    text += '\n'
-  }
-  return stackery.output({ text: text || 'No reactions recorded!' });
 }
--- Stackery/functions/Status/index.js
+++ Stackery/functions/Status/index.js
@@ -1,9 +1,14 @@
 const stackery = require('stackery');
 
-module.exports = function status(message) {
+module.exports = function reportGenerator(message) {
   return stackery.output({ action: 'select' }, { port: 0 }).then((result) => {
-    return stackery.output({ users: result[0].records }, { port: 1 }).then((result) =>  {
-      return result;
-    });
+    let report = '';
+    let users = result[0].records;
+    for (let user of users) {
+      report += `<@${user.id}>: `;
+      report += user.reactions.map((rxn) => `:${reaction.type}:`).join('');
+      report += '\n'
+    }
+    return stackery.output({ text: report });
   });
 }
