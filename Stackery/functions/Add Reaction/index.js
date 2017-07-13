b4695c6a9dcebbab72ffbe15dadbb43bb1633ca6
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-07-12 18:18:29 -0700

    Trying to understand events and the easiest way to respond to reaction_removed

--- Stackery/functions/Add Reaction/index.js
+++ Stackery/functions/Add Reaction/index.js
@@ -2,17 +2,11 @@ const stackery = require('stackery')
 
 module.exports = function reactionCounter(event) {
   return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
-    console.log('records are', result);
-    console.log('event is', event);
     let user = result[0].records[0] ? result[0].records[0] : { id: event.item_user, reactions: [] };
     user.reactions.push({ 
-      type: event.reaction, 
-      timestamp: parseFloat(event.item.ts), 
+      type: event.reaction,
+      timestamp: parseFloat(event.event_ts), 
     });
-    console.log('putting', user);
-    return stackery.output({ action: 'put', record: user }).then((result) => {
-      console.log('result from putting is ', result);
-      return {};
-    });
+    return stackery.output({ action: 'put', record: user }).then((result) => ({}));
   });
 }
--- Stackery/functions/Remove Reaction/index.js
+++ Stackery/functions/Remove Reaction/index.js
@@ -1,14 +1,9 @@
 const stackery = require('stackery')
 
-module.exports = function reactionCounter(message) {
-  const event = JSON.parse(message.body.toString()).event;
+module.exports = function reactionCounter(event) {
   return stackery.output({ action: 'select', where: { id: event.item_user } }).then((result) => {
-    console.log('records are', result);
     let user = result[0].records[0] ? result[0].records[0] : { id: event.item_user, reactions: [] };
-    user.reactions.push({ type: event.reaction });
-    console.log('putting', user);
-    stackery.output({ action: 'put', record: user }).then((result) => {
-      return {};
-    });
+    console.log('Event is ', event);
+    return {}
   });
 }
