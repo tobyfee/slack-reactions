d5927590f5f004440671c9544c27c64c78b17971
Sam Goldstein <sgrock@gmail.com>
2018-01-22 16:23:39 -0800

    Update ingest function

--- Stackery/functions/Ingest/index.js
+++ Stackery/functions/Ingest/index.js
@@ -1,21 +1,33 @@
-const stackery = require('stackery')
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
 
-module.exports = function eventRouter(message) {
-  const body = JSON.parse(message.body.toString());
-  if (body.token != process.env.VERIFICATION_TOKEN) {
+module.exports = function eventRouter (event, context, callback) {
+  const body = JSON.parse(event.body.toString());
+  if (body.token !== process.env.VERIFICATION_TOKEN) {
     throw new Error('Invalid Verification Token received from Slack!');
   }
   switch (body.type) {
     case 'url_verification':
-      return body.challenge;
+      return callback(null, {
+        body: body.challenge,
+        statusCode: 200
+      });
     case 'event_callback':
-      // Here, we use Stackery's waitFor parameter to specify that we
+      // Here, we use Lambda's 'event' invocation parameter to specify that we
       // want to return as soon as the message is transmitted, rather
-      // than waiting for the promise to resolve, because we need to
-      // make sure that we respond to Slack within three seconds
-      return stackery.output(body, {waitFor: 'TRANSMISSION'})
-        .then(() => ({ statusCode: 204 }));
+      // than waiting for the remote lambda to respond, because we need to
+      // make sure that we respond to Slack within three seconds.
+      const params = {
+        FunctionName: JSON.parse(process.env.STACKERY_PORTS[0][0]).functionName,
+        InvocationType: 'Event',
+        Payload: JSON.stringify(body)
+      };
+      lambda.invoke(params).promise().then(callback(null, {
+        statusCode: 204,
+        body: ''
+      }));
+      break;
     default:
-      throw new Error('Unrecognized event type: ', body);
+      callback(new Error('Unrecognized event type: ', body));
   }
-}
+};
